e15f5064baeea4f06447b3690cc93b2f6fb92e54
garlic0x1 <garlic1@protonmail.com>
2023-10-19 08:46:40 -0500

    validate tags

--- src/render.lisp
+++ src/render.lisp
@@ -4,7 +4,8 @@
   (format nil "~v@{~A~:*~}" depth "  "))
 
 (defun format-attr (attr)
-  (format nil "~(~a~)=\"~a\"" (car attr) (cdr attr)))
+  (let ((k (car attr)) (v (cdr attr)))
+    (format nil "~(~a~)=\"~a\"" k v)))
 
 (defgeneric render (sxml &key &allow-other-keys)
   (:method ((sxml symbol) &key out (depth 0))
@@ -18,7 +19,7 @@
             (escape sxml #'escape-table)))
 
   (:method ((sxml list) &key out (depth 0))
-    (let* ((tag (car sxml))
+    (let* ((tag (validate-tag (car sxml)))
            (attrs (loop :for (k v) :on (cdr sxml) :by 'cddr
                         :while (keywordp k)
                         :collect (cons k v)))
