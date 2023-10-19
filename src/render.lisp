bb782bf7717bf392d7834dbcb16e11a2bc5c03f6
garlic0x1 <garlic1@protonmail.com>
2023-10-19 07:35:26 -0500

    indenting

--- src/render.lisp
+++ src/render.lisp
@@ -1,34 +1,32 @@
 (in-package :hiccl)
 
+(defun indent (depth)
+  (format nil "~v@{~A~:*~}" depth "  "))
+
+(defun format-attr (attr)
+  (format nil "~(~a~)=\"~a\"" (car attr) (cdr attr)))
+
 (defgeneric render (sxml &key &allow-other-keys)
-  (:method ((sxml symbol) &key out)
-    (format out "~a" sxml))
+  (:method ((sxml symbol) &key out (depth 0))
+    (format out "~a~a"
+            (indent depth)
+            sxml))
 
-  (:method ((sxml string) &key out)
-    (format out "~a" (escape sxml #'escape-table)))
+  (:method ((sxml string) &key out (depth 0))
+    (format out "~a~a"
+            (indent depth)
+            (escape sxml #'escape-table)))
 
-  (:method ((sxml list) &key out)
+  (:method ((sxml list) &key out (depth 0))
     (let* ((tag (car sxml))
            (attrs (loop :for (k v) :on (cdr sxml) :by 'cddr
                         :while (keywordp k)
                         :collect (cons k v)))
            (children (nthcdr (+ 1 (* 2 (length attrs))) sxml)))
-      (format out "<~(~a~)~{ ~a~}>~%~{~a~%~}</~(~a~)>"
+      (format out "~a<~(~a~)~{ ~a~}>~{~%~a~}~%~a</~(~a~)>"
+              (indent depth)
               tag
-              (mapcar (lambda (attr) (format nil "~(~a~)=\"~a\"" (car attr) (cdr attr))) attrs)
-              (mapcar #'render children)
-              tag)))
-
-  ;; not as convenient as i thought, since CL vecs are deep quoted unlike clj
-  ;; (:method ((sxml vector) &key out)
-  ;;   (let* ((tag (first-elt sxml))
-  ;;          (attrs (loop :for batch :in (batches (subseq sxml 1) 2)
-  ;;                       :while (keywordp (first-elt batch))
-  ;;                       :collect batch))
-  ;;          (children (subseq sxml (+ 1 (* 2 (length attrs))))))
-  ;;     (format out "<~(~a~) ~{~a~^ ~}>~{~a~}</~(~a~)>"
-  ;;             tag
-  ;;             (mapcar (lambda (attr) (format nil "~(~a~)=\"~a\"" (elt attr 0) (elt attr 1))) attrs)
-  ;;             (map 'list #'render children)
-  ;;             tag)))
-  )
+              (mapcar #'format-attr attrs)
+              (mapcar (lambda (child) (render child :out out :depth (+ 1 depth))) children)
+              (indent depth)
+              tag))))
