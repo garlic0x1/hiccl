e5fd09031288e51941612d395c8911a3e27ae5da
garlic0x1 <garlic1@protonmail.com>
2023-10-31 00:15:45 -0500

    fix attribute sanitization

--- src/utils.lisp
+++ src/utils.lisp
@@ -5,12 +5,6 @@
 
 (defun curry (f x) (lambda (&rest args) (apply f (cons x args))))
 
-(defun format-attr (attr)
-  (let ((k (car attr)) (v (cdr attr)))
-    (if v
-        (format nil "~(~a~)=\"~a\"" k v)
-        (format nil "~(~a~)" k))))
-
 (defun extract-attrs-and-children (body)
   (let* ((attrs (loop :for (k v) :on body :by 'cddr
                       :while (keywordp k)
