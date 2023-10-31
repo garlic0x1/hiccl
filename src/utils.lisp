16f91b85050625145afd4023aedf22c4d822c3d5
garlic0x1 <garlic1@protonmail.com>
2023-10-30 23:25:38 -0500

    bool attrs

--- src/utils.lisp
+++ src/utils.lisp
@@ -6,7 +6,10 @@
 (defun curry (f x) (lambda (&rest args) (apply f (cons x args))))
 
 (defun format-attr (attr)
-  (let ((k (car attr)) (v (cdr attr))) (format nil "~(~a~)=\"~a\"" k v)))
+  (let ((k (car attr)) (v (cdr attr)))
+    (if v
+        (format nil "~(~a~)=\"~a\"" k v)
+        (format nil "~(~a~)" k))))
 
 (defun extract-attrs-and-children (body)
   (let* ((attrs (loop :for (k v) :on body :by 'cddr
